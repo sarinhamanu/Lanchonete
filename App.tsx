63cc4f000a4e9ddc1334c8462a8d35f8d5abcb60
sarinhamanu <sarasejbrs@gmail.com>
2024-03-21 15:52:55 -0300

    atua

--- App.tsx
+++ App.tsx
@@ -1,14 +1,15 @@
 import React  from 'react';
 import { FlatList, ScrollView } from 'react-native';
 import ScrollViewExample from './src/ScrollViewExample';
-import  FlatListExample  from './src/FlatListExample'
-import  DoceListExample from './src/DoceExample'
+import  FlatListExample  from './src/FlatListExample';
+import  DoceListExample from './src/DoceExample';
+import  Cadastro  from'./src/Cadastro';
 
 function App(): React.JSX.Element{
 
 
 return(
- <DoceListExample/>
+ <Cadastro/>
 );
 
 }
