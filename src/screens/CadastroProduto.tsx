6d5b42cca211016da46b21dc913a3dfaa4e74c82
sarinhamanu <sarasejbrs@gmail.com>
2024-04-02 13:48:19 -0300

    atua

---
+++ src/screens/CadastroProduto.tsx
@@ -0,0 +1,80 @@
+import React, { useState }  from "react";
+import { StyleSheet, View } from "react-native";
+
+const CadastroProduto: React.FC = () =>{
+    const[produtos, setProdutos]= useState<Produto[]>([]);
+    const[nome, setNome]= useState<string>('');
+    const[preco,SetPreco]= useState<number>(0);
+    const[ingredientes, setIngredientes]= useState<string>('');
+    const[imagem,setImagem]= useState<any>('');
+
+    const cadastrarProduto = async ()=>{
+
+    }
+    return(
+        <View>
+
+        </View>
+    );
+}
+const styles =StyleSheet.create({
+
+    container:{
+        flex: 1
+    },
+    header:{
+    backgroundColor:'red',
+    },
+    headerText:{
+        fontSize: 20,
+        fontWeight:'bold',
+        color:'white',
+    },
+    form:{
+    padding:10,
+    backgroundColor:'#f0f0f0',
+    marginBottom:10
+    },
+    input:{
+        height:40,
+        borderColor:'gray',
+        borderWidth:1,
+        marginBottom:10,
+        paddingHorizontal:10,
+        borderRadius:10
+    },
+    imageButton:{
+    backgroundColor:'red',
+    padding:10,
+    borderRadius:5,
+    alignItems:'center',
+    marginBottom:10
+    },
+    imageButtonText:{
+        color:'white',
+        fontWeight:'bold',
+    },
+    imagemSelecionada:{
+        width:200,
+        height:200,
+        resizeMode:'cover',
+        borderRadius:5,
+        marginBottom:10,
+
+    },
+    alinhamentoimagemSelecionada:{
+        alignItems:'center',
+    },
+    button:{
+        backgroundColor:'red',
+        padding:10,
+        borderRadius:5,
+        alignItems:'center'
+    },
+    buttonText:{
+        color:'white',
+        fontWeight:'bold',
+    }
+})
+
+export default CadastroProduto;
